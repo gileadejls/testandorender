ddcffe5d4f9af0a7af8bc78392a139349afa54f9
Gileadejls <gileadelima16@gmail.com>
2023-11-21 11:15:47 -0300

    adicionando tailwindcss e criando rota login

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ const app = express()
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
+//ROTA CSS
+app.use(express.static(path.join(__dirname, 'public')));
 
+//ROTAS
+
+//HOME
 app.get('/', (req, res)=>{
     res.render('index.ejs')
+})
+
+//LOGIN
+app.get('/login', (req, res)=>{
+    res.render('login')
 })
 
 app.listen(3000, ()=>{
