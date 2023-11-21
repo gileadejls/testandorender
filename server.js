f3b4bf26516709ea242824d99eaeae2d3686f372
Gileadejls <gileadelima16@gmail.com>
2023-11-21 11:00:50 -0300

    adicionando EJS

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 
-app.get('/', (req, res)=>{
-    res.send('Ola Mundo, Ola Render')
-})
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
 
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+})
 
 app.listen(3000, ()=>{
     console.log('Servidor rodando')
